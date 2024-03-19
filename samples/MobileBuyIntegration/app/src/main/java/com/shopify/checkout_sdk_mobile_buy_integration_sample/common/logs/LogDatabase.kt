80aee1a2d27145e5d66673a54e6d2ef45261ca3c
Daniel Kift <daniel.kift@shopify.com>
2024-03-19 14:28:42 +0000

    small sample app tweaks, use same param name as super in migration, switch icon, use param

--- samples/MobileBuyIntegration/app/src/main/java/com/shopify/checkout_sdk_mobile_buy_integration_sample/common/logs/LogDatabase.kt
+++ samples/MobileBuyIntegration/app/src/main/java/com/shopify/checkout_sdk_mobile_buy_integration_sample/common/logs/LogDatabase.kt
@@ -39,7 +39,7 @@ abstract class LogDatabase : RoomDatabase() {
 }
 
 val MIGRATION_1_2 = object : Migration(1, 2) {
-     override fun migrate(database: SupportSQLiteDatabase) {
-          database.execSQL("ALTER TABLE LogLine ADD COLUMN checkout_completedorderDetails TEXT")
+     override fun migrate(db: SupportSQLiteDatabase) {
+          db.execSQL("ALTER TABLE LogLine ADD COLUMN checkout_completedorderDetails TEXT")
      }
 }
