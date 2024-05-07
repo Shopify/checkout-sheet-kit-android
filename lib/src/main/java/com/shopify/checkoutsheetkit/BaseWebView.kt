11d389dfbcf7775058f5ce0f5e529e6aba2a32be
Daniel Kift <daniel.kift@shopify.com>
2024-05-07 11:59:49 +0100

    deal with customer acc required

--- lib/src/main/java/com/shopify/checkoutsheetkit/BaseWebView.kt
+++ lib/src/main/java/com/shopify/checkoutsheetkit/BaseWebView.kt
@@ -40,6 +40,7 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import java.net.HttpURLConnection.HTTP_GONE
 import java.net.HttpURLConnection.HTTP_NOT_FOUND
+import java.net.HttpURLConnection.HTTP_UNAUTHORIZED
 
 @SuppressLint("SetJavaScriptEnabled")
 internal abstract class BaseWebView(context: Context, attributeSet: AttributeSet? = null) :
@@ -162,6 +163,15 @@ internal abstract class BaseWebView(context: Context, attributeSet: AttributeSet
                             )
                         )
                     }
+                    errorCode == HTTP_UNAUTHORIZED &&  responseHeaders[SHEET_KIT_ERROR_HEADER]?.lowercase() == CUSTOMER_ACCOUNT_REQUIRED -> {
+                        processor.onCheckoutViewFailedWithError(
+                            AuthenticationException(
+                                errorDescription = "Customer account required.",
+                                isRecoverable = false,
+                                errorCode = AuthenticationException.CUSTOMER_ACCOUNT_REQUIRED,
+                            )
+                        )
+                    }
                     errorCode == HTTP_GONE -> processor.onCheckoutViewFailedWithError(
                         CheckoutExpiredException(
                             isRecoverable = false,
@@ -182,7 +192,9 @@ internal abstract class BaseWebView(context: Context, attributeSet: AttributeSet
 
     companion object {
         private const val DEPRECATED_REASON_HEADER = "X-Shopify-API-Deprecated-Reason"
+        private const val SHEET_KIT_ERROR_HEADER = "X-Shopify-Checkout-Sheet-Kit-Error"
         private const val LIQUID_NOT_SUPPORTED = "checkout_liquid_not_supported"
+        private const val CUSTOMER_ACCOUNT_REQUIRED = "customer_account_required"
 
         private const val TOO_MANY_REQUESTS = 429
         private val CLIENT_ERROR = 400..499
