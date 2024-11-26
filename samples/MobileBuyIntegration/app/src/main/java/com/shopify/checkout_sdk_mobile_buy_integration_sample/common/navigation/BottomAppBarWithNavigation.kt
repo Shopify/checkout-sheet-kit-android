356d745a9c24a74a9c3f4664864fe4db14447f7f
Dan Kift <daniel.kift@shopify.com>
2024-11-26 13:33:34 +0000

    Add text description under bottom nav bar icons (#155)

--- samples/MobileBuyIntegration/app/src/main/java/com/shopify/checkout_sdk_mobile_buy_integration_sample/common/navigation/BottomAppBarWithNavigation.kt
+++ samples/MobileBuyIntegration/app/src/main/java/com/shopify/checkout_sdk_mobile_buy_integration_sample/common/navigation/BottomAppBarWithNavigation.kt
@@ -23,21 +23,27 @@
 package com.shopify.checkout_sdk_mobile_buy_integration_sample.common.navigation
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.shopify.checkout_sdk_mobile_buy_integration_sample.R
+import com.shopify.checkout_sdk_mobile_buy_integration_sample.common.components.BodySmall
 
 @Composable
 fun BottomAppBarWithNavigation(
@@ -55,21 +61,21 @@ fun BottomAppBarWithNavigation(
                 navController,
                 Screen.Home,
                 ImageVector.vectorResource(R.drawable.home),
-                "Home",
+                stringResource(id = R.string.navigation_home),
                 currentScreen,
             )
             NavigationItem(
                 navController,
                 Screen.Products,
                 ImageVector.vectorResource(R.drawable.product),
-                "Products",
+                stringResource(id = R.string.navigation_shop),
                 currentScreen,
             )
             NavigationItem(
                 navController,
                 Screen.Settings,
                 ImageVector.vectorResource(R.drawable.profile),
-                "Settings",
+                stringResource(id = R.string.navigation_log_in),
                 currentScreen,
             )
         }
@@ -83,21 +89,30 @@ fun NavigationItem(
     icon: ImageVector,
     label: String,
     currentScreen: Screen,
-    badge: @Composable () -> Unit = {}
 ) {
-    IconButton(
-        onClick = { navController.navigate(screen.route) },
-        modifier = Modifier.semantics {
-            this.contentDescription = "$label icon"
-        }
-    ) {
-        val tint = if (currentScreen == screen) {
-            MaterialTheme.colorScheme.primary
-        } else {
-            Color.Unspecified
+    val isActiveScreen = currentScreen == screen
+    val color = if (isActiveScreen) {
+        MaterialTheme.colorScheme.primary
+    } else {
+        Color.Unspecified
+    }
+
+    Column {
+        IconButton(
+            onClick = { navController.navigate(screen.route) },
+            modifier = Modifier.semantics {
+                this.contentDescription = "$label icon"
+            }
+        ) {
+            Icon(imageVector = icon, contentDescription = label, tint = color)
         }
 
-        Icon(imageVector = icon, contentDescription = label, tint = tint)
-        badge()
+        BodySmall(
+            text = label,
+            color = color,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .offset(0.dp, (-7.5).dp)
+        )
     }
 }
