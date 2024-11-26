a39e807c7093f69645fd64dc8b9f42e9d79cb179
Daniel Kift <daniel.kift@shopify.com>
2024-11-26 11:27:51 +0000

    tweaks

--- samples/MobileBuyIntegration/app/src/main/java/com/shopify/checkout_sdk_mobile_buy_integration_sample/common/components/TextComponents.kt
+++ samples/MobileBuyIntegration/app/src/main/java/com/shopify/checkout_sdk_mobile_buy_integration_sample/common/components/TextComponents.kt
@@ -29,6 +29,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
@@ -178,6 +179,7 @@ fun MoneyText(
         color = color,
         textAlign = textAlign,
         text = toDisplayText(
+            LocalContext.current,
             currency,
             price,
             locale,
