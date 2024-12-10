c72a4902cc3e7a4cfd5c3321988f87a6ac01dad5
Dan Kift <daniel.kift@shopify.com>
2024-12-10 21:34:16 +0000

    [Sample App] Update RemoteImage to fallback to a placeholder (#164)

--- samples/MobileBuyIntegration/app/src/main/java/com/shopify/checkout_sdk_mobile_buy_integration_sample/common/components/RemoteImage.kt
+++ samples/MobileBuyIntegration/app/src/main/java/com/shopify/checkout_sdk_mobile_buy_integration_sample/common/components/RemoteImage.kt
@@ -22,30 +22,41 @@
  */
 package com.shopify.checkout_sdk_mobile_buy_integration_sample.common.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import coil.size.Scale
+import com.shopify.checkout_sdk_mobile_buy_integration_sample.R
 
 @Composable
 fun RemoteImage(
-    url: String,
-    altText: String,
+    url: String?,
+    altText: String?,
     modifier: Modifier
 ) {
-    AsyncImage(
-        model = ImageRequest.Builder(LocalContext.current)
-            .data(url)
-            .scale(Scale.FILL)
-            .crossfade(true)
-            .build(),
-        contentScale = ContentScale.Crop,
-        alignment = Alignment.Center,
-        contentDescription = altText,
-        modifier = modifier,
-    )
+    if (url != null) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(url)
+                .scale(Scale.FILL)
+                .crossfade(true)
+                .build(),
+            contentScale = ContentScale.Crop,
+            alignment = Alignment.Center,
+            contentDescription = altText,
+            modifier = modifier,
+        )
+    } else {
+        Image(
+            painter = painterResource(id = R.drawable.placeholder),
+            contentDescription = altText,
+            modifier = modifier,
+        )
+    }
 }
