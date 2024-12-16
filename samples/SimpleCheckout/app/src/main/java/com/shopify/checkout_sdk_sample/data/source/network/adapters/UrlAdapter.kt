630330c6adc06aac2ee083089196c4fd868b68bc
Daniel Kift <daniel.kift@shopify.com>
2024-12-16 10:45:17 +0000

    license headers

--- samples/SimpleCheckout/app/src/main/java/com/shopify/checkout_sdk_sample/data/source/network/adapters/UrlAdapter.kt
+++ samples/SimpleCheckout/app/src/main/java/com/shopify/checkout_sdk_sample/data/source/network/adapters/UrlAdapter.kt
@@ -1,3 +1,25 @@
+/*
+ * MIT License
+ *
+ * Copyright 2023-present, Shopify Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package com.shopify.checkout_sdk_sample.data.source.network.adapters
 
 import com.apollographql.apollo.api.Adapter
@@ -18,9 +40,9 @@ class URLAdapter : Adapter<URL> {
         return try {
             URI(urlString!!).toURL()
         } catch (e: URISyntaxException) {
-            throw IllegalStateException("Invalid URI format: $urlString", e)
+            throw RuntimeException("Invalid URI format: $urlString", e)
         } catch (e: MalformedURLException) {
-            throw IllegalStateException("Invalid URL format: $urlString", e)
+            throw RuntimeException("Invalid URL format: $urlString", e)
         }
     }
 
