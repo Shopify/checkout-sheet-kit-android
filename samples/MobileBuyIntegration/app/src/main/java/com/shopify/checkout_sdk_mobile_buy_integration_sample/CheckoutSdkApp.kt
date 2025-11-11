9e80138f08cebe9f8bf1b111173468199794133d
Daniel Kift <daniel.kift@shopify.com>
2025-11-11 14:06:47 +0000

    Show address picker, clear up unused imports

--- samples/MobileBuyIntegration/app/src/main/java/com/shopify/checkout_sdk_mobile_buy_integration_sample/CheckoutSdkApp.kt
+++ samples/MobileBuyIntegration/app/src/main/java/com/shopify/checkout_sdk_mobile_buy_integration_sample/CheckoutSdkApp.kt
@@ -25,7 +25,6 @@ package com.shopify.checkout_sdk_mobile_buy_integration_sample
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
@@ -53,7 +52,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalResources
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -65,6 +64,7 @@ import com.shopify.checkout_sdk_mobile_buy_integration_sample.common.SnackbarCon
 import com.shopify.checkout_sdk_mobile_buy_integration_sample.common.navigation.BottomAppBarWithNavigation
 import com.shopify.checkout_sdk_mobile_buy_integration_sample.common.navigation.CheckoutSdkNavHost
 import com.shopify.checkout_sdk_mobile_buy_integration_sample.common.navigation.Screen
+import com.shopify.checkout_sdk_mobile_buy_integration_sample.common.ui.CheckoutEventHandler
 import com.shopify.checkout_sdk_mobile_buy_integration_sample.common.ui.theme.CheckoutSdkSampleTheme
 import com.shopify.checkout_sdk_mobile_buy_integration_sample.logs.LogsViewModel
 import com.shopify.checkout_sdk_mobile_buy_integration_sample.settings.SettingsUiState
@@ -94,8 +94,7 @@ fun CheckoutSdkAppRoot(
 
     val cartState = cartViewModel.cartState.collectAsState()
     val totalQuantity = cartState.value.totalQuantity
-    val context = LocalContext.current
-
+    val resources = LocalResources.current
     CheckoutSdkSampleTheme(darkTheme = useDarkTheme) {
         Surface(
             modifier = Modifier.fillMaxSize(),
@@ -108,7 +107,7 @@ fun CheckoutSdkAppRoot(
             ObserveAsEvents(flow = SnackbarController.events) { event ->
                 scope.launch {
                     snackbarHostState.currentSnackbarData?.dismiss()
-                    snackbarHostState.showSnackbar(message = context.resources.getText(event.resourceId).toString())
+                    snackbarHostState.showSnackbar(message = resources.getText(event.resourceId).toString())
                 }
             }
 
@@ -196,6 +195,8 @@ fun CheckoutSdkAppRoot(
                     )
                 }
             }
+
+            CheckoutEventHandler()
         }
     }
 }
