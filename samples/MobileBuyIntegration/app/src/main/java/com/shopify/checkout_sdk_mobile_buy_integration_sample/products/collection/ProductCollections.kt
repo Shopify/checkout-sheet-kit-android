08f7575f8fe0f05df0a60af8de7f7058329f8e93
Dan Kift <daniel.kift@shopify.com>
2026-04-20 11:16:16 +0100

    bump-mat3-and-replace-icon-usage (#528)

--- samples/MobileBuyIntegration/app/src/main/java/com/shopify/checkout_sdk_mobile_buy_integration_sample/products/collection/ProductCollections.kt
+++ samples/MobileBuyIntegration/app/src/main/java/com/shopify/checkout_sdk_mobile_buy_integration_sample/products/collection/ProductCollections.kt
@@ -29,14 +29,13 @@ import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.shopify.checkout_sdk_mobile_buy_integration_sample.R
@@ -109,7 +108,7 @@ fun ProductCollection(
                 modifier = Modifier.padding(top = 10.dp)
             )
             Icon(
-                imageVector = Icons.AutoMirrored.Filled.ArrowForward,
+                painter = painterResource(id = R.drawable.arrow_forward),
                 contentDescription = stringResource(id = R.string.collection_cta_content_description),
                 modifier = Modifier.padding(start = 5.dp, top = 10.dp)
             )
